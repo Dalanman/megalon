36797b39dcbfce81a0336de9780f68f465f5a622
DlnLimited <dylanlmt11@gmail.com>
2025-05-17 17:35:51 +0800

    feat: :zap: create login page front-end
    created the ui for gmail log in
    
    still needs to be connected to backend

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,32 @@
-
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { GmailButton } from "@/components/ui/gmailButton";
+import { Mail } from "lucide-react";
 
 export default function Home() {
   return (
-    <div>
-
+    <div className="flex items-center justify-center min-h-screen w-full">
+      <Card className="w-100">
+        <CardHeader>
+          <CardTitle className="text-xl"> Get started with Megalon.</CardTitle>
+          <CardDescription>
+            {" "}
+            Streamline customer support responses{" "}
+          </CardDescription>
+        </CardHeader>
+        <div className="flex justify-center">
+          <Mail />
+        </div>
+        <div className="flex justify-center p-4">
+          <GmailButton />
+        </div>
+      </Card>
     </div>
   );
 }
